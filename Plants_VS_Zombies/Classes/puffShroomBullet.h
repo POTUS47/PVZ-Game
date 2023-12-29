6e9b780f26211f176121db1ebaa28fe1156709fe
xvwenhan <xvwenhan@outlook.com>
2023-12-29 17:06:05 +0800

    清空重开

--- Plants_VS_Zombies/Classes/puffShroomBullet.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "cocos2d.h"
-#include "Bullet.h"
-class puffShroomBullet : public Bullet {
-public:
-    puffShroomBullet(int scale,int _row, int startX, int startY, int bulletDamage, Scene* scene);
-    void explodeAnimation();
-};
